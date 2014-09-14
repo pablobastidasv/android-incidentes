8d1b222e8d592b4d4cb1378e25535c402d770e65
Mitzy Valencia <mitzy.19@hotmail.com>
2014-09-14 06:18:34 -0500

    nothing

--- SysHelp/app/src/main/java/com/syshelp/app/activities/InfoListInc.java
+++ SysHelp/app/src/main/java/com/syshelp/app/activities/InfoListInc.java
@@ -24,7 +24,7 @@ import java.util.ArrayList;
 public class InfoListInc extends Activity {
 
     private String message;
-    private SeguimientoVO seguimientoVO;
+    //private SeguimientoVO seguimientoVO;
     private SeguimientoAdapter adptSeg;
     /**
      *
